c6d11dd641ea40f9d587bea0f6d8f5f655a6ee8f
Patrick N <patricknnoonan@gmail.com>
2019-04-17 09:22:38 -0500

    init

---
+++ particlestest.js
@@ -0,0 +1,91 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 600,
+        density: { enable: true, value_area: 552.4033491425909 }
+      },
+      color: { value: "#ffffff" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 100 }
+      },
+      opacity: {
+        value: 1,
+        random: true,
+        anim: { enable: true, speed: 1, opacity_min: 0, sync: false }
+      },
+      size: {
+        value: 2,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 7.192807192807193,
+          size_min: 3,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: false,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 0.5,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 600 }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "grab" },
+        onclick: { enable: true, mode: "bubble" },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 71.92807192807193,
+          line_linked: { opacity: 0.534278844986279 }
+        },
+        bubble: {
+          distance: 71.92807192807193,
+          size: 7.992007992007992,
+          duration: 0.3196803196803197,
+          opacity: 0.04795204795204795,
+          speed: 3
+        },
+        repulse: { distance: 63.93606393606394, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 }
+      }
+    },
+    retina_detect: true
+  });
+  /* -- counts particles and fps --
+  var count_particles, stats, update;
+  stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = "absolute";
+  stats.domElement.style.left = "0px";
+  stats.domElement.style.top = "0px";
+  document.body.appendChild(stats.domElement);
+  count_particles = document.querySelector(".js-count-particles");
+  update = function() {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
+  */
